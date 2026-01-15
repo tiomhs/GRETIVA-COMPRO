3415bba1543ad26c0aeee60c6f04f31c44e731d7
Tio Mahesa <tiomahesa.work@gmail.com>
2026-01-16 05:18:20 +0800

    fix: update base path in Vite config for consistent local development

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [vue()],
   // Ganti '/nama-repository-kamu/' sesuai nama repo di GitHub.
   // Kalau nanti pakai domain sendiri (gretiva.com), ubah jadi '/'
-  base: process.env.NODE_ENV === 'production' ? '/GRETIVA-COMPRO/' : '/', 
+  // base: process.env.NODE_ENV === 'production' ? '/GRETIVA-COMPRO/' : '/', 
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
